a6daf28e452b519abfa186148859307ad958274d
David Apple <davidapple@protonmail.com>
2016-04-07 15:26:21 +0100

    Gulp application built with json upload fallback if json load fails.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,180 @@
 'use strict';
 
 (function(){
-
-    window.zxcvbn = zxcvbn;
     window.davemail = new Object();
     davemail.info = 'Davemail 0.0.1 by David Apple https://github.com/davidapple/davemail Donate Bitcoin to 13D3A8PP91MLF5VTBQMH5HG76F42RNRF28';
 
+    // - Functions
+    function loadDavemail(){
+        return $.getJSON('davemail.json', function() {
+            console.log('davemail.json loaded confirmation one');
+        })
+            .done(function() {
+                console.log('davemail.json loaded confirmation two');
+            })
+            .fail(function( jqxhr, textStatus, error ) {
+                var err = textStatus + ", " + error;
+                console.log('davemail.json failed to load: ' + err );
+                $('#signIn').hide();
+                $('#importDavemail').show();
+            });
+    }
+
+    function loadDavemailAgain(davemail){
+        var newDavemail = $.getJSON('davemail.json', function() {
+            console.log('davemail.json loaded confirmation one');
+        })
+            .done(function(response) {
+                console.log('davemail.json loaded confirmation two');
+                if(_.isEqual(davemail.jsonData.responseJSON, response)){
+                    $('#usernameHeading').text(davemail.username);
+                    messagesPage();
+                    davemail.messages = mapMessages(davemail);
+                    buildMessagesTable(davemail);
+                }else{
+
+                    // Tell user file has not beed replaced
+                    console.log('Hey, listen, you havent replaced the file dude.');
+                    $('#fileNotReplacedWarning').show();
+                    $('#fileReplacedLoader').hide();
+                    $('#fileReplacedButton').show();
+
+                }
+            })
+            .fail(function( jqxhr, textStatus, error ) {
+                var err = textStatus + ", " + error;
+                console.log('davemail.json failed to load: ' + err );
+            });
+    }
+
+    function signInPage(event){
+        if(_.isObject(event)){
+            event.preventDefault();
+        }
+        $('#signIn').show();
+        $('#signUp').hide();
+        $('#messages').hide();
+        $('#signUpReplace').hide();
+        $('#navSignInButton').parent().addClass('active');
+        $('#navSignUpButton').parent().removeClass('active');
+        $('#navMessagesButton').parent().removeClass('active');
+    }
+    function signUpPage(event){
+        if(_.isObject(event)){
+            event.preventDefault();
+        }
+        $('#signUp').show();
+        $('#signIn').hide();
+        $('#messages').hide();
+        $('#navSignUpButton').parent().addClass('active');
+        $('#navSignInButton').parent().removeClass('active');
+        $('#navMessagesButton').parent().removeClass('active');
+    }
+    function messagesPage(event){
+        if(_.isObject(event)){
+            event.preventDefault();
+        }
+        $('#messages').show();
+        $('#signIn').hide();
+        $('#signUp').hide();
+        $('#signUpReplace').hide();
+        $('#navMessagesButton').show();
+        $('#navMessagesButton').parent().addClass('active');
+        $('#navSignInButton').parent().removeClass('active');
+        $('#navSignUpButton').parent().removeClass('active');
+        $('#navSignOutButton').show();
+        $('#navSignInButton').hide();
+        $('#navSignUpButton').hide();
+        $('.jumbotron').hide();
+    }
+    function signOutPage(event){
+        if(_.isObject(event)){
+            event.preventDefault();
+        }
+        $('#signIn').show();
+        $('#signInButton').show();
+        $('#messages').hide();
+        $('#signUp').hide();
+        $('#signUpReplace').hide();
+        $('#navMessagesButton').hide();
+        $('#navSignInButton').parent().addClass('active');
+        $('#navMessagesButton').parent().removeClass('active');
+        $('#navSignUpButton').parent().removeClass('active');
+        $('#navSignInButton').show();
+        $('#navSignUpButton').show();
+        $('#navSignOutButton').hide();
+        $('.jumbotron').show();
+    }
+    function signOut(davemail){
+        davemail.messagesTable.destroy();
+        davemail.messagesTable = $('#messagesTable').DataTable({
+            data: [],
+            ordering: false,
+            columns: [
+                {title: "Time"},
+                {title: "Message"}
+            ]
+        });
+        davemail.username = undefined;
+        davemail.password = undefined;
+        davemail.passwordStrength = undefined;
+        davemail.publicKey = undefined;
+        davemail.privateKey = undefined;
+        davemail.messages = undefined;
+        return davemail;
+    }
+    function generateKeyPair(davemail){
+        davemail.privateKey = cryptico.generateRSAKey(davemail.password, 1536);
+        davemail.publicKey = cryptico.publicKeyString(davemail.privateKey);
+        console.log(davemail.privateKey);
+        console.log(davemail.publicKey);
+        return davemail;
+    }
+
+    function mapMessages(davemail){
+        return _.map(davemail.jsonData.responseJSON.davemail.emails, function(num, key){
+            var message = cryptico.decrypt(num.cipher, davemail.privateKey);
+            if (message.status == 'success'){
+                console.log(message);
+                return [ num.time, message.plaintext ];
+            }
+        });
+    }
+
+    function buildMessagesTable(davemail){
+        if (!_.isUndefined(davemail.messages[0])){
+            davemail.messagesTable.destroy();
+            davemail.messagesTable = $('#messagesTable').DataTable({
+                data: davemail.messages,
+                ordering: false,
+                columns: [
+                    {title: "Time"},
+                    {title: "Message"}
+                ]
+            });
+        }
+    }
+
+    // - Application
+
+    // Load davemail.json
     davemail.jsonData = loadDavemail();
+
+    // Let users upload davemail.json if it fails to load
+    $(document).on('change', '#importDavemailFile', function(event){
+        var reader = new FileReader();
+        $('#importDavemailButton').hide();
+        $('#importingLoader').show();
+        reader.onload = function(event){
+            console.log('davemail.json loaded confirmation one');
+            var jsonObj = JSON.parse(event.target.result);
+            davemail.jsonData = new Object();
+            davemail.jsonData.responseJSON = jsonObj;
+            $('#signIn').show();
+            $('#importDavemail').hide();
+        }
+        reader.readAsText(event.target.files[0]);
+    });
 
     $('#navSignInButton').click(function(event){
         signInPage(event);
